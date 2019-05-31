14d9c51ee2b741e86849d74e20f9164ed5d82d50
Hector <7esting@gmail.com>
2019-05-30 22:49:42 -0700

    Update to models

--- microblog.py
+++ microblog.py
@@ -1,4 +1,11 @@
 from app import app
 
+from app.models import User, Post
+
+# Use from cli: flask shell
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=False)
+    app.run(host='0.0.0.0', port=80, debug=Fal
